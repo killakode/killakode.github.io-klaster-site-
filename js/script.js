a517fe328f13eaebf28d357a32b31e414188220d
killaсode(Nikita) <sudarev.n@yandex.ru>
2024-04-08 18:57:01 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+  const menuNavItems = document.querySelectorAll('.main__nav__item');
+
+  // Функция для изменения активного пункта меню
+  function changeActiveMenuItem(item) {
+    // Удаляем класс 'active' со всех пунктов меню
+    menuNavItems.forEach(item => item.classList.remove('active'));
+    // Добавляем класс 'active' к выбранному пункту меню
+    item.classList.add('active');
+  }
+
+  // Добавляем обработчики событий к каждому пункту меню
+  menuNavItems.forEach(item => {
+    item.addEventListener('click', () => changeActiveMenuItem(item));
+  });
+
+
+
+  const menuItems = document.querySelectorAll('.menu__item');
+
+  // Функция для изменения активного пункта меню
+  function changeActiveMenuItem(item) {
+    // Удаляем класс 'active' со всех пунктов меню
+    menuItems.forEach(item => item.classList.remove('active'));
+    // Добавляем класс 'active' к выбранному пункту меню
+    item.classList.add('active');
+  }
+
+  // Добавляем обработчики событий к каждому пункту меню
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => changeActiveMenuItem(item));
+  });
+
+
 function initializeSlickSlider() {
   if (!$('.requirements__cards').hasClass('slick-initialized')) {
     $('.requirements__cards').slick({
